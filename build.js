2573f78a6fb3ca8574ccb663456e39235dfd22c7
Esteban Lussich <esteban.lussich@autodesk.com>
2015-12-10 18:20:25 -0800

    First working version.
    - Added `tachyons-flex.css` source file.
    - Added `build.js` script, and generated distribution files from there.
    - Updated README file.
    - Added `index.html` file, displaying working examples and valuable
    documentation.
    - Updated `package.json` file, accordingly.

---
+++ build.js
@@ -0,0 +1,34 @@
+// dependencies
+
+var fs = require("fs")
+var autoprefixer = require("autoprefixer")
+var postcss = require("postcss")
+var atImport = require("postcss-import")
+var cssvariables = require('postcss-css-variables')
+var compressor = require('node-minify')
+var conditionals = require('postcss-conditionals')
+var customMedia = require("postcss-custom-media")
+
+// css to be processed
+var css = fs.readFileSync("src/tachyons-flex.css", "utf8")
+
+// process css
+var output = postcss([autoprefixer])
+  .use(atImport())
+  .use(cssvariables())
+  .use(conditionals())
+  .use(customMedia())
+  .process(css, {
+    from: "src/tachyons-flex.css",
+    to: "tachyons-flex.css"
+  })
+  .css
+
+fs.writeFile("tachyons-flex.css", output, 'utf-8')
+
+// Using YUI Compressor for CSS
+new compressor.minify({
+    type: 'sqwish',
+    fileIn: 'tachyons-flex.css',
+    fileOut: 'tachyons-flex.min.css'
+});
